2376561c1b159c22e5df1d9f09359d2918b9cb01
liangjunjian <2641711397@qq.com>
2024-06-10 23:09:05 +0800

    修改路由mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
@@ -36,6 +36,11 @@ const router = createRouter({
       path: '/gsap/timeline',
       name: 'timeline',
       component: () => import('../views/gsap/timeline.vue')
+    },
+    {
+      path: '/cancelToken',
+      name: 'cancelToken',
+      component: () => import('../views/cancelToken.vue')
     },
   ]
 })
