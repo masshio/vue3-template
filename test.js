8a788972b30cca328025709d8d56b031608903e8
liangjunjian <2641711397@qq.com>
2024-06-10 23:01:14 +0800

    提交测试代码

---
+++ test.js
@@ -0,0 +1,9 @@
+function debounce(fn, delay) {
+    let timer = null
+    return function (...args) {
+        if(timer) clearTimeout(timer)
+        timer = setTimeout(() => {
+            fn(...args)
+        }, delay)
+    }
+}
